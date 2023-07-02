702cc8f8019e21642e37e4d19d942e99415ae507
seav1 <107227866+seav1@users.noreply.github.com>
2023-07-02 23:32:55 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,10 +1,10 @@
 #!/bin/bash
 # X-ray设置，可以不修改
-UUID='f0177922-2dcc-4c0f-819c-7b74b7bbbfac'
-VMPATH='vm123456'
-VLPATH='vl123456'
+UUID='db5b4014-d2da-11ed-afa1-0242ac120002'
+VMPATH='vm'
+VLPATH='vl'
 # A-ro隧道设置，必须填
-TOK='eyJhIxxxxxxxxxxxxxxxxCJ9'
+TOK='eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiMDNjZDQ4ZTMtOTYxMi00YzNjLTg3ODMtMjk4ODg2NmY1MDJhIiwicyI6Ik1HUTFaV05oTnpZdFl6UXdZaTAwTUROaExUbGlNMkl0T1RoaE5qTmhNR0UxTURreiJ9'
 # 哪吒设置，不需要就删掉
 NEZHA_SERVER='xxxxxxg'
 NEZHA_KEY='MxxxxxxxxxI'
@@ -14,6 +14,6 @@ NEZHA_PORT=${NEZHA_PORT:-'443'}
 # 哪吒tls开关,1开启0关闭
 NEZHA_TLS=${NEZHA_TLS:-'1'}
 # X-ray下载链接
-URL_BOT='github.com/dsadsadsss/plutonodes/releases/download/xr/kano'
+URL_BOT='github.com/seav1/waifly/blob/main/web.js'
 # 文件下载路径
 paths='/tmp/app'
