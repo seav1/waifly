4abac8cae7660e3c1f488f8ddb5a644b43d08822
seav1 <107227866+seav1@users.noreply.github.com>
2023-07-04 12:27:13 +0800

    Update c.py

--- c.py
+++ c.py
@@ -1,8 +1,8 @@
 #!/bin/bash
 # X-ray设置，可以不修改
 UUID='db5b4014-d2da-11ed-afa1-0242ac120002'
-VMPATH='vm'
-VLPATH='vl'
+VMPATH='/vm'
+VLPATH='/vl'
 # A-ro隧道设置，必须填
 TOK='eyJhIjoiZjAzMGY1ZDg4OGEyYmRlN2NiMDg3NTU5MzM4ZjE0OTciLCJ0IjoiMDNjZDQ4ZTMtOTYxMi00YzNjLTg3ODMtMjk4ODg2NmY1MDJhIiwicyI6Ik1HUTFaV05oTnpZdFl6UXdZaTAwTUROaExUbGlNMkl0T1RoaE5qTmhNR0UxTURreiJ9'
 # 哪吒设置，不需要就删掉
